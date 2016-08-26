3bd370c722eeab0d26fab69a92ef30565b5c7ed8
Wolfgang Criollo <wolfplisken@gmail.com>
2016-08-26 15:33:02 -0400

    created controller and routes

---
+++ todo-list/app/controllers/tasks_controller.rb
@@ -0,0 +1,14 @@
+class TasksController < ApplicationController
+  def new
+  end
+
+  def show
+    task = Task.all
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
---
+++ todo-list/test/controllers/tasks_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class TasksControllerTest < ActionController::TestCase
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
+
+  test "should get update" do
+    get :update
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get :delete
+    assert_response :success
+  end
+
+end
