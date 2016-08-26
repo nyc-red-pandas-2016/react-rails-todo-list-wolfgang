0e47184bc508e112e518dcad24c4c9b34730cb50
Wolfgang Criollo <wolfplisken@gmail.com>
2016-08-26 17:43:28 -0400

    create conection between react front end and backend rails successfully got a json response

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,15 +1,45 @@
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Axios from "axios";
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(){
+    super();
+      this.state = {
+        tasks: []
+      }
+    }
+    componentDidMount(){
+      Axios.get("http://localhost:3000/tasks/show")
+        .then((response)=>{
+            this.setState({
+              tasks:response.data
+            })
+          // end of then
+        })
+        .catch((errors)=>{
+          console.log(errors  )
+          // end of catch
+        })
+    }
+    render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <ul>
+         {this.state.tasks.map((task,index)=>{
+          return(
+            <li key={index}>
+                {task.name}
+            </li>
+            )
+         })}
+         </ul>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
