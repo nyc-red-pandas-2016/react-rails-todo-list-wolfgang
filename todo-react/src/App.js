d42fbc2fca479608035719c1072e9a1d63c9a205
Wolfgang Criollo <wolfplisken@gmail.com>
2016-08-27 19:25:49 -0400

    created tasks component and a task component and pass data down to display

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Axios from "axios";
-import task from './components/Task';
+import Tasks from './components/Tasks';
 import './App.css';
 
 class App extends Component {
@@ -32,18 +32,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <ul>
-         {this.state.tasks.map((task,index)=>{
-          return(
-            <li key={index}>
-                {task.name}
-            </li>
-            )
-         })}
-         </ul>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Tasks data={this.state.tasks}/>
       </div>
     );
   }
--- todo-react/src/components/Task.js
+++ todo-react/src/components/Task.js
@@ -3,8 +3,14 @@ import React,{Component} from "react";
 
 export default class Task extends Component{
   render(){
+    let {name,description,created_at,updated_at} = this.props.data
     return(
-        
+      <div className="task-info">
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <sub>created:{created_at} Updated:{updated_at}</sub>
+      </div>
+
     // end of
        )
 
